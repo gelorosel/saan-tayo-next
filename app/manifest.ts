7fd771d7eae812e24419c93d26d28e8aec1737c6
Gelo Rosel <gelo.rosel@gmail.com>
2026-02-01 23:40:50 +0800

    Purpose: any

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/images/default-img.jpeg',
         sizes: '192x192',
         type: 'image/jpeg',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
       {
         src: '/images/default-img.jpeg',
         sizes: '512x512',
         type: 'image/jpeg',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
     ],
   };
